3a52c62d28fe983f119961ed653c96ce96d67c65
charlesxu27 <charlesxu27@gmail.com>
2022-06-09 22:31:12 +0200

    Add seeMore and hover styles

--- script.js
+++ script.js
@@ -1,52 +1,111 @@
 // Instantiate constants and global variables
 const API_KEY = "2bc0bfedf2889bc7dfd1db360b7b6f27"
-// https://api.themoviedb.org/3/movie/550?api_key=2bc0bfedf2889bc7dfd1db360b7b6f27
-// https://api.themoviedb.org/3/search/movie?api_key=2bc0bfedf2889bc7dfd1db360b7b6f27&language=en-US&query=dog&page=1&include_adult=false
+
 let page = 1
 let searchTerm = ""
 
 // Create DOM references
 
+contentTitleElement = document.querySelector("#content-title")
 movieGridElement = document.querySelector("#movies-grid")
 submitButtonElement = document.querySelector("#submit-button")
 searchInputElement = document.querySelector("#search-input")
+seeMoreButtonElement = document.querySelector("#load-more-movies-btn")
 
 // Create event listeners
+submitButtonElement.addEventListener("click", (event) => {
+    contentTitleElement.classList.add("hidden")  // clear header on new search query
+    movieGridElement.innerHTML = ``  // clear existing results on new search query
+    page = 1
+    handleSearchSubmit(event, page)
+})
 
-submitButtonElement.addEventListener("click", handleSearchSubmit)
+searchInputElement.addEventListener("keydown", (event) => {
+    contentTitleElement.classList.add("hidden")
+    movieGridElement.innerHTML = ``  // clear existing results on new search query
+    page = 1
+    if (event.key === "Enter") {
+        submitButtonElement.click() // trigger button click
+    }
+})
 
-async function getMovies(event, searchTerm) {
+seeMoreButtonElement.addEventListener("click", (event) => {
+    page += 1
+    console.log("*** calling handleSearchSubmit for Page: " + page)
+    handleSearchSubmit(event, page)
+})
+
+// Main functions
+async function getMovies(event, searchTerm, page) {
     console.log("*** Calling getMovies***")
     event.preventDefault() // override submit button behavior (refresh page)
     let url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}&page=${page}&include_adult=false`
+    console.log("API URL: " + url)
     const response = await fetch(url)
     const data = await response.json()
     console.log("*** Getting Data ***")
     console.log(data)
-    movieGridElement.innerHTML = ``  // clear existing results on api call
     return data
 }
+
 
 function displayMovies(data) {
     console.log("*** Calling displayMovies ***")
     data.results.forEach(movie => {
         if (movie.poster_path === null) {
             movieGridElement.innerHTML += `
-            <img src="./assets/placeholder_img.jpg">
+            <div class="movie-card">
+              <img class="movie-poster" src="./assets/placeholder_img.jpg" alt="placeholder image">
+              <span class="movie-title">${movie.original_title}</span>
+              <span class="movie-votes">${ratingToStars(movie.vote_average)}</span>
+            </div>
             `
-        }
+        } else {
         movieGridElement.innerHTML += `
-        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+        <div class="movie-card">
+          <img class="movie-poster" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.original_title}">
+          <span class="movie-title">${movie.original_title}</span>
+          <span class="movie-votes">${ratingToStars(movie.vote_average)}</span>
+        </div>
         `
-    });
+        }
+    })
 }
 
-async function handleSearchSubmit(event) {
+
+async function handleSearchSubmit(event, page) {
     console.log("*** Calling handleSearchSubmit ***")
     searchTerm = searchInputElement.value
-    const data = await getMovies(event, searchTerm)
+    const data = await getMovies(event, searchTerm, page)
+    seeMoreButtonElement.classList.remove("hidden")  // reveal seeMoreButton after results load
+    
     displayMovies(data)
-    searchInputElement.value = ""  // clear search bar after query
-
+    // searchInputElement.value = ""  // clear search bar after query
 }
 
+
+function ratingToStars(rating) {
+    if (rating === null || rating == 0) {
+        return "N/A"
+    }
+    result = ""
+    rating = Math.ceil(rating/2)
+    for (let i = 0; i < rating; i++) {
+        result += "&#11088; "
+    }
+    return result
+}
+
+async function loadMainPage(event, page) {
+    console.log("*** Calling loadMainPage ***")
+    url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
+    response = await fetch(url)
+    data = await response.json()
+    displayMovies(data)
+    seeMoreButtonElement.classList.add("hidden")  // hide seeMoreButton on main page
+}
+
+
+window.onload = function() {
+    loadMainPage(1)
+}
